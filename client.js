ac00d2503bd174067f5e2382cb50157bd9ec6da9
Abuzar <abuzarshabab@gmail.com>
2021-10-10 18:45:52 +0530

    Working ClI for Submit

--- client.js
+++ client.js
@@ -20,26 +20,42 @@ socket.on('message', (data) => {
       const cmd = data;
       console.log(chalk.green( cmd.split('\n')[0]));
   })
+socket.on('move' ,(data) =>{
+    console.log(chalk.redBright( data));
+    console.log(chalk.green('it\'s your turn'));
+})
+
+socket.on('win',() => {
+    console.log('🏆🏆🥇  Hurray You Won   🥇🏆🏆');
+})
+
+socket.on('loose',() => {
+    console.log('😭😭 You Loose : Try again  😭😭 ');
+})
+
 
 socket.on('input',()=>{
     const readline = require('readline').createInterface({
     input: process.stdin
     })
 
-    readline.question(`What's your name?`, name => {
+    readline.question("", name => {
     socket.send(name);
     })
-
-
 })
 
+socket.on('playerAssign' , (count) =>{
+    console.log(`Your are ${ count === 1 ? 'first' : 'second'} player`);
+})
 
 // This function take tic tac toe element and show in pattern
 function showBoard(board) {
-     board.forEach((val) =>{
+    console.log(chalk.yellow('------------------\n'));
+     board.forEach((val,index) =>{
         process.stdout.write(chalk.greenBright(val+'\t'));
-        if(val % 3 === 0){
+        if((index+1) % 3 === 0){
             console.log('\n');
         }
     })
+         console.log(chalk.yellow('------------------'));
 }
