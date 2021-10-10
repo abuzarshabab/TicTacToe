bc187cdb4557d21812735deef347a9725c637705
Abuzar <abuzarshabab@gmail.com>
2021-10-10 16:51:38 +0530

    Two way chat with

--- client.js
+++ client.js
@@ -1,1 +1,45 @@
-const socket = require('socket.io-client');
+const socket = require('socket.io-client')('http://localhost:5050');
+const repl = require ('repl');
+const chalk = require('chalk');
+
+
+socket.on('disconnect' , () =>{
+  cons('disconnect');
+})
+
+socket.on('info' , (msg) =>{
+    console.log(chalk.green(msg));
+});
+
+socket.on('showBoard',(board)=>{
+   showBoard(board);
+})
+  
+ 
+socket.on('message', (data) => {
+      const cmd = data;
+      console.log(chalk.green( cmd.split('\n')[0]));
+  })
+
+socket.on('input',()=>{
+    const readline = require('readline').createInterface({
+    input: process.stdin
+    })
+
+    readline.question(`What's your name?`, name => {
+    socket.send(name);
+    })
+
+
+})
+
+
+// This function take tic tac toe element and show in pattern
+function showBoard(board) {
+     board.forEach((val) =>{
+        process.stdout.write(chalk.greenBright(val+'\t'));
+        if(val % 3 === 0){
+            console.log('\n');
+        }
+    })
+}
--- server.js
+++ server.js
@@ -1,2 +1,56 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
+const PORT = 5050;
+var counter= 1;
+const players = ['X','O'];
+var board = [1,2,3,4,5,6,7,8,9];
+
+io.on('connection', (socket)=>{
+
+    console.log(`Player ${counter} is connected`);
+
+     socket.emit('info',(`Connected to 127.0.0.1 ${PORT}`));
+
+     socket.emit('input');
+
+     if(counter == 2){
+        // If Two Players gets connected Then show board event will be emmited
+        io.sockets.emit('showBoard',(board));
+        p2 = socket.id;
+        counter=1;
+
+    }else{
+        p1= socket.id;
+        counter++;
+    }
+
+    socket.on('message', (evt) => {   
+            socket.emit('input');
+            socket.broadcast.emit('message', evt)
+        })
+
+})
+
+
+io.on('disconnect',(disc)=>{
+    socket.broadcast.emit('disconnect');
+    console.log(`Player is disconnected`);
+
+})
+
+server.listen(PORT , () => {
+    console.log(`Server is listening on localhost : ${PORT}`);
+})
+
+
+
+
+
+
+
+// p1 = id 
+// p2 = id
+
+// if(p[0] sends val) =>{
+//     board[val-1] <= p[0]
+// }
