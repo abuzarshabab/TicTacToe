c5ca9d1be2b8996366d24c8b2d5b62cc3f36b058
Abuzar <abuzarshabab@gmail.com>
2021-10-11 12:29:18 +0530

    Fixed Handling next Players : after Player Finish the Game

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,14 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 const process = require("process");
+
 const isWin = require("./isWin");
 const PORT = process.argv[2];
 
 let counter = 1;
-
+let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let movesCounter = 0;
 io.on("connection", (socket) => {
-  let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   console.log(`Player ${counter} is connected`);
   socket.emit("info", `Connected to 127.0.0.1 ${PORT}`);
 
@@ -50,9 +50,11 @@ io.on("connection", (socket) => {
         io.sockets.emit("showBoard", board);
         socket.emit("win");
         socket.broadcast.emit("loose");
+        board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
       } else if (movesCounter >= 9) {
         io.sockets.emit("showBoard", board);
         io.sockets.emit("draw");
+        board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
       } else {
         io.sockets.emit("showBoard", board);
         socket.broadcast.emit("move", `Opponent Move Position : ${evt}`);
